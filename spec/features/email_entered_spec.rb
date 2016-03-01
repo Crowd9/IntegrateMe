721bd9a82a960992e7a694f12901f3f57dc9dac7
Ali Graham <ali.graham@gmail.com>
2016-03-01 16:04:37 +1030

    feature specs

--- spec/features/email_entered_spec.rb
+++ spec/features/email_entered_spec.rb
@@ -2,20 +2,33 @@ require 'rails_helper'
 
 feature 'email entered' do
 
+  let(:competition_data) { {id: 5, name: "Email only comp", requires_entry_name: false} }
+
+  let(:valid_email) { 'jdoe@example.org' }
+  let(:invalid_email) { 'jdoe' }
+
   before do
-    skip
+    Competition.create!(competition_data)
+    visit '/'
+    click_link(competition_data[:name])
   end
 
   scenario 'with valid email' do
-
+    fill_in('Email', :with => valid_email)
+    click_on('Enter!')
+    expect(page).to have_content('Thank you for entering our competition!')
   end
 
   scenario 'with blank email' do
-
+    fill_in('Email', :with => '')
+    click_on('Enter!')
+    expect(page).to have_content('Sorry, there was a problem saving your entry:')
   end
 
   scenario 'with invalid email' do
-
+    fill_in('Email', :with => invalid_email)
+    click_on('Enter!')
+    expect(page).to have_content('Sorry, there was a problem saving your entry:')
   end
 
 end
--- spec/features/name_and_email_entered_spec.rb
+++ spec/features/name_and_email_entered_spec.rb
@@ -2,28 +2,58 @@ require 'rails_helper'
 
 feature 'name and email entered' do
 
+  let(:competition_data) { {id: 6, name: "Name+Email comp", requires_entry_name: true} }
+
+  let(:name) { 'Jane Doe' }
+  let(:valid_email) { 'jdoe@example.org' }
+  let(:invalid_email) { 'jdoe' }
+
   before do
-    skip
+    Competition.create!(competition_data)
+    visit '/'
+    click_link(competition_data[:name])
   end
 
   scenario 'with name and valid email' do
-
+    fill_in('Name', :with => name)
+    fill_in('Email', :with => valid_email)
+    click_on('Enter!')
+    expect(page).to have_content('Thank you for entering our competition!')
   end
 
-  scenario 'with blank name and valid email' do
-
-  end
-
-  scenario 'with blank name and blank email' do
-
+  scenario 'with name and blank email' do
+    fill_in('Name', :with => name)
+    fill_in('Email', :with => '')
+    click_on('Enter!')
+    expect(page).to have_content('Sorry, there was a problem saving your entry:')
   end
 
   scenario 'with name and invalid email' do
+    fill_in('Name', :with => name)
+    fill_in('Email', :with => invalid_email)
+    click_on('Enter!')
+    expect(page).to have_content('Sorry, there was a problem saving your entry:')
+  end
 
+  scenario 'with blank name and valid email' do
+    fill_in('Name', :with => '')
+    fill_in('Email', :with => valid_email)
+    click_on('Enter!')
+    expect(page).to have_content('Sorry, there was a problem saving your entry:')
+  end
+
+  scenario 'with blank name and blank email' do
+    fill_in('Name', :with => '')
+    fill_in('Email', :with => '')
+    click_on('Enter!')
+    expect(page).to have_content('Sorry, there was a problem saving your entry:')
   end
 
   scenario 'with blank name and invalid email' do
-
+    fill_in('Name', :with => '')
+    fill_in('Email', :with => invalid_email)
+    click_on('Enter!')
+    expect(page).to have_content('Sorry, there was a problem saving your entry:')
   end
 
 end
