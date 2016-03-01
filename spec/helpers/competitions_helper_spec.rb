5d03ac8619d4a13e17ed2a5df73b35728b6869cf
Ali Graham <ali.graham@gmail.com>
2016-03-01 15:17:41 +1030

    minor cleanup

--- spec/helpers/competitions_helper_spec.rb
+++ spec/helpers/competitions_helper_spec.rb
@@ -3,8 +3,12 @@ require "rails_helper"
 describe CompetitionsHelper do
   describe "#competition_entrant_page" do
     it "creates permalinks for competitions" do
-      competition = Competition.create!(id: 99, name: "My Competition!")
-      expect(helper.competition_entrant_page(competition)).to eq("/99/my-competition")
+      competition = double(Competition)
+      expect(competition).to receive(:id).and_return(99)
+      expect(competition).to receive(:name).and_return('My Competition!')
+
+      page_link = helper.competition_entrant_page(competition)
+      expect(page_link).to eq("/99/my-competition")
      end
   end
 end
